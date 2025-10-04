4465557a6ab8e22f84978169938f18e8bc869cb2
Antonio Emmanuele Renda <134848500+anemre@users.noreply.github.com>
2025-10-05 00:03:17 +0200

    test container build action

--- size_diff.py
+++ size_diff.py
@@ -1,7 +1,7 @@
 """Tool to monitor LoC differences"""
 
 
-# inspired by https://github.com/tinygrad/tinygrad/blob/master/sz.py
+# Inspired by https://github.com/tinygrad/tinygrad/blob/master/sz.py
 
 #####################################################################################################################################
 #                                                           Module Setup                                                            #
